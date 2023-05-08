d60bab03aa32b667d4493f17632a4c89a4ac6c64
Yash Anand <122223761+Anand11Yash@users.noreply.github.com>
2023-05-08 12:47:24 +0530

    Update CarbonFootprintCalculator.c

--- CarbonFootprintCalculator.c
+++ CarbonFootprintCalculator.c
@@ -13,10 +13,10 @@ int main(){
     printf("Press enter to continue");
     getchar();
 
-    printf("\nThis calculator was created as the pre-existing Carbon Footprint calculators appeared to have a limitation with their criterias in their calculations.\n\n");
-    printf("Additionally, none of these calculators were OpenSource, which means that they would not allow the user to add/remove specific activities to/from the calculator's criterias\n\n");
-    printf("This lack of freedom to modify the criterias as per user's needs, would make the results incomplete and inaccurate.\n\n");
-    printf("This is how the creation of this Carbon Footprint Calculator came into being where we welcome all to make modifications to help make it even better.\n");
+    printf("\nThis calculator was created as the pre-existing Carbon Footprint calculators appeared to have a limitation with their criteria in their calculations.\n");
+    printf("Additionally, none of these calculators were OpenSource, which means that they would not allow the user to add/remove specific activities to/from the calculator's criteria\n");
+    printf("This lack of freedom to modify the criteria as per user's needs, would make the results incomplete and inaccurate.\n");
+    printf("This is how the creation of this Carbon Footprint Calculator came into being where we welcome all to make positive modifications to help make it even better.\n");
 
     printf("\nAre you ready to calculate your Carbon Footprints?\n");
     printf("Press enter to continue");
