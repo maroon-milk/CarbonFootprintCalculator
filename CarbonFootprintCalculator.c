ff8c1d6be266bf2ff67200ceebf8d36fef572e86
Yash Anand <122223761+Anand11Yash@users.noreply.github.com>
2023-05-18 10:44:40 +0530

    Negative no longer allowed

--- CarbonFootprintCalculator.c
+++ CarbonFootprintCalculator.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
 
 int main(){
     
+        
     float ac, Electricity, TV, cocacola, solarenergy;   //Domestic related consumption
     float CNG, LPG, diesel, petrol, LPGVEHC, coal;                                               //Resource consumption
     float beef, chicken, egg, fish, mutton, pork, veg, milk, foodwastage, water, cookingoil; //Food consumption
@@ -29,19 +33,27 @@ int main(){
     getchar();
 
     printf("\nQ01. How much AIR CONDITIONER do you use kWh/month?\n(As a general rule, an AC of 1 ton uses 1 kWh, 2 ton uses 2 kWh, 3 ton uses 3 kWh of electricity per hour.\n");
-    scanf("%f", &ac);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", ac * 0.241);
+    while (scanf("%f", &ac) != 1 || ac < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);
+    } printf("The C02e Emmision of your consumption is %f Kg/Month\n", ac * 0.241);
     
     printf("\nQ02. How much in kWh/month is your ELECTRICITY consumption?\n");
-    scanf("%f", &Electricity);
-    printf("Your C02e Emmision of your consumption is %f Kg/Month\n", Electricity * 0.72);
+    while (scanf("%f", &Electricity) != 1 || Electricity < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("Your C02e Emmision of your consumption is %f Kg/Month\n", Electricity * 0.82);
 
     printf("\nQ03. How much TV do you use kWh/month?\n(Most TVs use 0.08 to 0.4 kWh/Hour\n");
-    scanf("%f", &TV);
+    while (scanf("%f", &TV) != 1 || TV < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", TV * 0.088);
 
     printf("\nQ04. How much solar energy do you consume from SOLAR PANELS in kWh/Month\n");
-    scanf("%f", &solarenergy);
+    while (scanf("%f", &solarenergy) != 1 || solarenergy < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", solarenergy * 0.041);
 
     printf("\n* * * * * * * * * * * * * * * * *\n");
@@ -51,32 +63,46 @@ int main(){
     getchar();
     
     printf("\nQ05. How much CNG do you consume Kg/month?\n");
-    scanf("%f", &CNG);
+    while (scanf("%f", &CNG) != 1 || CNG < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", CNG * 0.144);
 
     printf("\nQ06. How much PETROL do you consume Litres/month?\n");
-    scanf("%f", &petrol);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", petrol * 2.297);
+    while (scanf("%f", &petrol) != 1 || petrol < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", petrol * 2.3);
 
     printf("\nQ07. How much LPG do you consume Kg/month?\n");
-    scanf("%f", &LPG);
+    while (scanf("%f", &LPG) != 1 || LPG < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", LPG * 3.0);
 
     printf("\nQ08. How much DIESEL do you consume Litres/month?\n");
-    scanf("%f", &diesel);
+    while (scanf("%f", &diesel) != 1 || diesel < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", diesel * 2.6);
 
-    printf("\nQ09. How much LPG for your vehicle do you consume kg/month?\n");
-    scanf("%f", &LPGVEHC);  
+    printf("\nQ09. How much LPG for your vehicle do you consume kg/month?\n");  
+    while (scanf("%f", &LPGVEHC) != 1 || LPGVEHC < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", LPGVEHC * 3.06);
 
     printf("\nQ10. How much COAL do you consume Kg/Month?\n");
-    scanf("%f", &coal);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", coal * 2.42);
+    while (scanf("%f", &coal) != 1 || coal < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", coal * 2500000);
 
     printf("\nQ11. How much WATER do you use as general usage litres/month?\n(This includes drinking water)\n");
-    scanf("%f", &water);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", water * 0.000298);
+    while (scanf("%f", &water) != 1 || water < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", water * 0.298);
     
     printf("\n* * * * * * * * * * * * * * *\n");
     printf("* Round 3: Food Consumption *\n");
@@ -85,47 +111,69 @@ int main(){
     getchar();
 
     printf("\nQ12. How much BEEF do you consume Kg/Month?\n");
-    scanf("%f", &beef);
+    while (scanf("%f", &beef) != 1 || beef < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", beef * 27);
 
     printf("\nQ13. How much CHICKEN do you consume Kg/Month?\n");
-    scanf("%f", &chicken);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", chicken * 9.87);
+    while (scanf("%f", &chicken) != 1 || chicken < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", chicken * 6.9);
 
-    printf("\nQ14. How many EGGS do you consume Kg/Month?\n(1 eggs = 0.055Kg)\n");
-    scanf("%f", &egg);
+    printf("\nQ14. How many EGGS do you consume Kg/Month?\n(1Kg = 20 Eggs)\n");
+    while (scanf("%f", &egg) != 1 || egg < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", egg * 4.8);
 
     printf("\nQ15. How much FISH do you consume Kg/Month?\n");
-    scanf("%f", &fish);
+    while (scanf("%f", &fish) != 1 || fish < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", fish * 1.34);
 
     printf("\nQ16. How much MUTTON do you consume Kg/Month?\n");
-    scanf("%f", &mutton);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", mutton * 39.72);
+    while (scanf("%f", &mutton) != 1 || mutton < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", mutton * 39.2);
 
     printf("\nQ17. How much PORK do you consume Kg/month?\n");
-    scanf("%f", &pork);
+    while (scanf("%f", &pork) != 1 || pork < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", pork * 12.1);
 
     printf("\nQ18. How much VEGETABLES do you consume kg/Month?\n");
-    scanf("%f", &veg);
+    while (scanf("%f", &veg) != 1 || veg < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", veg * 2.0);
 
     printf("\nQ19. How much MILK do you consume Litres/Month?\n");
-    scanf("%f", &milk);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", milk * 1.39);
+    while (scanf("%f", &milk) != 1 || milk < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", milk * 0.91);
 
     printf("\nQ20. How much FOOD do you WASTE kg/Month?\n");
-    scanf("%f", &foodwastage);
+    while (scanf("%f", &foodwastage) != 1 || foodwastage < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", foodwastage * 2.5);
 
     printf("\nQ21. How much COCA-COLA do you consume Litres/Month\n");
-    scanf("%f", &cocacola);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", cocacola * 0.51);
+    while (scanf("%f", &cocacola) != 1 || cocacola < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", cocacola * 0.17);
 
     printf("\nQ22. How much COOKING OIL do you consume Litres/Month?\n");
-    scanf("%f", &cookingoil);
+    while (scanf("%f", &cookingoil) != 1 || cookingoil < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", cookingoil * 3.81);
 
 
@@ -136,34 +184,48 @@ int main(){
     getchar();
 
     printf("\nQ23. How much do you travel by MOTORCYCLE km/Month?\n");
-    scanf("%f", &motorcycle);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", motorcycle * 0.113);
+    while (scanf("%f", &motorcycle) != 1 || motorcycle < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", motorcycle * 0.08);
 
     printf("\nQ24. How much do you travel by AEROPLANE Km/Month?\n");
-    scanf("%f", &aeroplane);
+    while (scanf("%f", &aeroplane) != 1 || aeroplane < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", aeroplane * 0.146);
 
     printf("\nQ25. How much do you travel by BUS Km/Month?\n");
-    scanf("%f", &bus);
+    while (scanf("%f", &bus) != 1 || bus < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", bus * 0.103);
 
     printf("\nQ26. How much do you travel by TRAIN Km/Month?\n");
-    scanf("%f", &train);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", train * 0.37);
+    while (scanf("%f", &train) != 1 || train < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", train * 0.10);
 
     printf("\nQ27. How much do you travel by AUTORICKSHAW Km/Month?\n");
-    scanf("%f", &autorickshaw);
+    while (scanf("%f", &autorickshaw) != 1 || autorickshaw < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
     printf("The C02e Emmision of your consumption is %f Kg/Month\n", autorickshaw*0.05);
 
     printf("\nQ28. How much do you travel by CAR Km/Month?\n");
-    scanf("%f", &car);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", car * 0.111);
+    while (scanf("%f", &car) != 1 || car < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", car * 0.31);
 
     printf("\nQ29. How much distance do you travel by BICYCLE in Km/Month?\n");
-    scanf("%f", &bicycle);
-    printf("The C02e Emmision of your consumption is %f Kg/Month\n", bicycle * 0.021);    
+    while (scanf("%f", &bicycle) != 1 || bicycle < 0) {
+        printf("Invalid input. Please try again.\n");
+        fflush(stdin);}
+    printf("The C02e Emmision of your consumption is %f Kg/Month\n", bicycle * 0.0205052);    
     
-    CarbonFootprint = ((Electricity*0.72) + (water*0.000298) + (ac * 0.241) + (TV * 0.088) + (LPG*3.0) + (cocacola * 0.51) + (solarenergy * 0.041) + (CNG * 0.144) + (petrol * 2.297) + (diesel * 2.6) + (LPGVEHC*3.06) + (coal*2.52) + (beef*27) + (chicken * 9.87) + (egg * 4.8) + (fish * 1.34) + (mutton*39.72) + (pork*12.1) + (veg*2) + (milk*1.39) + (foodwastage*2.5) + (cookingoil * 3.81) + (motorcycle*0.113) + (aeroplane*0.146) + (bus*0.103) + (train*0.37) + (autorickshaw*0.05) + (car*0.111) + (bicycle*0.021));
+    CarbonFootprint = ((Electricity*0.82) + (water*0.298) + (ac * 0.241) + (TV * 0.088) + (LPG*3.0) + (cocacola * 0.17) + (solarenergy * 0.041) + (CNG * 0.144) + (petrol * 2.3) + (diesel * 2.6) + (LPGVEHC*3.06) + (coal*2500000) + (beef*27) + (chicken * 6.9) + (egg * 4.8) + (fish * 1.34) + (mutton*39.2) + (pork*12.1) + (veg*2) + (milk*0.91) + (foodwastage*2.5) + (cookingoil * 3.81) + (motorcycle*0.08) + (aeroplane*0.146) + (bus*0.103) + (train*0.10) + (autorickshaw*0.05) + (car*0.31) + (bicycle*0.0205052));
 
     printf("\nYour carbon footprint is %f Kg per Month.\n", CarbonFootprint);
     printf("Press ENTER To Compare Your Result With India's Per Capita Carbon Footprint\n");
@@ -173,5 +235,6 @@ int main(){
     printf("\n\nThank You For Using Yash Anand's Carbon Footprint Calculator!\n");
     printf("Press enter to continue");
     getchar();
+
     return 0;
-}
+    }
